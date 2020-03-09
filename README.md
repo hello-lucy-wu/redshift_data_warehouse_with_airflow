f11b8bbb22042773381a07b063084a085ebe09a9
Lucy <lucy@Lucys-MacBook-Air.local>
2020-03-08 22:54:20 -0400

    remove __pycache__ folder

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
  ### Commands
  - create a running container, use volumes that map the directories on local machine where DAG definitions and plugins are hold, and the locations where Airflow reads them on the container `docker run -d -p 8080:8080 -v /Users/lucy/dev/data-science-nanodegree/airflow-project/dags:/usr/local/airflow/dags -v /Users/lucy/dev/data-science-nanodegree/airflow-project/plugins/:/usr/local/airflow/plugins puckel/docker-airflow webserver`
  - show container info `docker ps`
+ - kill a container `docker stop <container id>`
  - jump into running container’s command line `docker exec -ti <container ID> bash`
  - test individual tests as part of DAG and logs the output to the command line `airflow test <dag name> <task name> 2020-03-08`
  
--- dags/redshift_data_warehouse.py
+++ dags/redshift_data_warehouse.py
@@ -18,54 +18,55 @@ from helpers import SqlQueries
 default_args = {
     'owner': 'lucy',
     'start_date': datetime(2020, 3, 8),
+    'depends_on_past': False, # The DAG does not have dependencies on past runs
+    'email': ['lucy.wu000@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': False, # Do not email on retry
+    'retries': 3, # On failure, the task are retried 3 times
+    'retry_delay': timedelta(minutes=5), # Retries happen every 5 minutes
 }
 
-dag = DAG('redshift_data_warehouse_dag',
+with DAG('redshift_data_warehouse_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
-        )
+          schedule_interval='0 * * * *',
+          catchup=False # Catchup is turned off
+        ) as dag:
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+    start_operator = DummyOperator(task_id='Begin_execution')
 
-stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
-    dag=dag
-)
+    stage_events_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_events'
+    )
+    stage_songs_to_redshift = StageToRedshiftOperator(
+        task_id='Stage_songs'
+    )
 
-stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
-    dag=dag
-)
+    load_songplays_table = LoadFactOperator(
+        task_id='Load_songplays_fact_table'
+    )
+    load_user_dimension_table = LoadDimensionOperator(
+        task_id='Load_user_dim_table'
+    )
+    load_song_dimension_table = LoadDimensionOperator(
+        task_id='Load_song_dim_table'
+    )
+    load_artist_dimension_table = LoadDimensionOperator(
+        task_id='Load_artist_dim_table'
+    )
+    load_time_dimension_table = LoadDimensionOperator(
+        task_id='Load_time_dim_table'
+    )
 
-load_songplays_table = LoadFactOperator(
-    task_id='Load_songplays_fact_table',
-    dag=dag
-)
+    run_quality_checks = DataQualityOperator(
+        task_id='Run_data_quality_checks'
+    )
 
-load_user_dimension_table = LoadDimensionOperator(
-    task_id='Load_user_dim_table',
-    dag=dag
-)
+    end_operator = DummyOperator(task_id='Stop_execution')
 
-load_song_dimension_table = LoadDimensionOperator(
-    task_id='Load_song_dim_table',
-    dag=dag
-)
+    start_operator >> [stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
 
-load_artist_dimension_table = LoadDimensionOperator(
-    task_id='Load_artist_dim_table',
-    dag=dag
-)
+    load_songplays_table >> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> run_quality_checks >> end_operator
+    
 
-load_time_dimension_table = LoadDimensionOperator(
-    task_id='Load_time_dim_table',
-    dag=dag
-)
 
-run_quality_checks = DataQualityOperator(
-    task_id='Run_data_quality_checks',
-    dag=dag
-)
-
-end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
